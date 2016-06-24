53b8583b7d5b03054dab24fcbfbb58b619c47fc4
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-06-24 18:49:28 +0300

    Added wonder behavior to make movement a bit better of Punkers.

---
+++ Assets/Game/Creatures/Behaviors/Wonder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Wonder : AIBehavior
+{
+    public float circleForward = 1f;
+    public float circleSize = 1f;
+    public float angleChangerPerSec = 15f;
+        
+    Vector3 displacement;
+
+    void Start()
+    {
+        displacement = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0, UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
+
+    public override Vector3[] GetDanger()
+    {
+        return AIBehavior.Empty;
+    }
+
+    public override Vector3[] GetInterest()
+    {
+
+        displacement = Quaternion.Euler(0, angleChangerPerSec * Time.deltaTime, 0) * displacement;
+
+        Vector3 circleCenter = transform.forward * circleForward;
+
+        Debug.DrawRay(this.transform.position + circleCenter, displacement, Color.red);
+
+        Vector3 final = circleCenter + (displacement * circleSize) * Strength;
+
+        //Debug.DrawRay(this.transform.position, final, Color.green);
+
+        //return AIBehavior.Empty;
+
+        return new Vector3[]
+        {
+            final
+        };
+    }
+}
