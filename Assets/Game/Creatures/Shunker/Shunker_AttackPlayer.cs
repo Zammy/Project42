cff3212a630e577411421240f3ac6ed32b036fc7
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-06-29 11:16:07 +0300

    Shunker wip

---
+++ Assets/Game/Creatures/Shunker/Shunker_AttackPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shunker_AttackPlayer : AttackPlayer 
+{
+    protected override void OnAttackFinished()
+    {
+        StateManager.ActivateState<Shunker_Wonder>();
+    }
+}
---
+++ Assets/Game/Creatures/Shunker/Shunker_StupifiedState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Shunker_StupifiedState : AIState 
+{
+    public float WaitForLeast = 0.15f;
+    public float WaitForMost = 0.30f;
+
+
+    float waitUntil = float.MaxValue;
+
+    public override void OnEnter(AIState previousState)
+    {
+        base.OnEnter(previousState);
+
+        waitUntil = Time.time + Random.Range(WaitForLeast, WaitForMost);
+
+        this.transform.DOLookAt( PlayerPos - CreatureTransform.position, 0.5f );
+    }
+
+	public override void StateUpdate()
+    {
+        if (Time.time > waitUntil)
+        {
+            this.StateManager.ActivateState<Shunker_SeekPlayer>();
+        }
+    }
+}
