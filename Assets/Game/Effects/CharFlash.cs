7d72baa9de9d0d9775ffde7e66df5a480640d08d
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-08-04 11:37:14 +0300

    Flashing before attack

---
+++ Assets/Game/Effects/CharFlash.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharFlash : MonoBehaviour 
+{
+    //Set through Unity
+    public Renderer Renderer;
+    public Material FlashMaterial;
+    //
+
+    Material material;
+
+    void Start()
+    {
+        material = Renderer.material;
+    }
+
+    public void BeforeAttack()
+    {
+        StartCoroutine( Flash());
+    }
+
+    IEnumerator Flash()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Renderer.material = FlashMaterial;
+
+            yield return null;
+            yield return null;
+
+            Renderer.material = material;
+
+            yield return null;
+            yield return null;
+
+        }
+    }
+}
